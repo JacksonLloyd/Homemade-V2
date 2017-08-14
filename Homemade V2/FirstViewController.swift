8f155d63c9ddf79125b4ff93775f0b648d3b404c
Joshua Jon <joshuajon@joshuas-macbook-pro.local>
2017-08-14 13:25:25 +1000

    Initial Commit

---
+++ Homemade V2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Homemade V2
+//
+//  Created by Joshua Jon on 14/8/17.
+//  Copyright © 2017 JoshuaJon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
