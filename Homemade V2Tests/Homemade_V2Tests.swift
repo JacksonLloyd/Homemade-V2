fc7ab41566fabc2f7a38a1ee1e8af601e9ce7934
JacksonLloyd <jacksonbrlloyd+github@gmail.com>
2017-09-03 22:42:25 +1000

    Final UITests

--- Homemade V2Tests/Homemade_V2Tests.swift
+++ Homemade V2Tests/Homemade_V2Tests.swift
@@ -24,6 +24,7 @@ class Homemade_V2Tests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
     }
     
     func testPerformanceExample() {
--- Homemade V2UITests/Homemade_V2UITests.swift
+++ Homemade V2UITests/Homemade_V2UITests.swift
@@ -33,4 +33,106 @@ class Homemade_V2UITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
+    func testfirstScene() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        element.tap()
+        element.swipeUp()
+        element.tap()
+        element.swipeRight()
+        element.swipeUp()
+        app.navigationBars["Homemade_V2.RecipeView"].buttons["backButton"].tap()
+        
+        let backbuttonButton = app.navigationBars["Homemade_V2.MealScene"].buttons["backButton"]
+        backbuttonButton.tap()
+        element.swipeDown()
+        element.swipeUp()
+        
+    }
+    func testSecondScene() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        app.tabBars.buttons["Cook Time"].tap()
+        
+        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        element.tap()
+        
+        let backbuttonButton = app.navigationBars["Homemade_V2.TimeTable"].buttons["backButton"]
+        backbuttonButton.tap()
+        element.tap()
+        element.tap()
+        element.swipeUp()
+        element.swipeUp()
+        element.tap()
+        app.navigationBars["Homemade_V2.RecipeView"].buttons["backButton"].tap()
+        app.navigationBars["Homemade_V2.MealScene"].buttons["backButton"].tap()
+        backbuttonButton.tap()
+        
+        
+    }
+    func testThirdScene() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        app.tabBars.buttons["Favorites"].tap()
+        
+        let element2 = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        let element = element2.children(matching: .other).element
+        element.tap()
+        element.swipeUp()
+        element.swipeUp()
+        element.swipeDown()
+        
+        let backbuttonButton = app.navigationBars["Homemade_V2.RecipeView"].buttons["backButton"]
+        backbuttonButton.tap()
+        
+        let backbuttonButton2 = app.navigationBars["Homemade_V2.MealScene"].buttons["backButton"]
+        backbuttonButton2.tap()
+        backbuttonButton2.tap()
+        element.tap()
+        element.swipeDown()
+        element.swipeUp()
+        
+    }
+    func testFourthScene() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        let tabBarsQuery = app.tabBars
+        let shoppingButton = tabBarsQuery.buttons["Shopping"]
+        shoppingButton.tap()
+        
+        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        element.tap()
+        element.tap()
+        element.swipeUp()
+        element.swipeUp()
+        element.swipeUp()
+        tabBarsQuery.buttons["Featured"].tap()
+        
+        let element2 = element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        element2.tap()
+        element2.swipeUp()
+        element2.tap()
+        shoppingButton.tap()
+        element.swipeUp()
+        element.tap()
+        element.tap()
+        element.tap()
+        element.tap()
+        element.tap()
+        element.tap()
+        element.tap()
+        element.tap()
+        element.tap()
+        element.tap()
+        
+    }
 }
